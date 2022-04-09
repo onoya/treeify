9f2256b39d97c440febd18af1e4c0bb0092f9301
Naoya Ono <ono.naoyaa@gmail.com>
2022-04-09 17:50:27 +0800

    initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { load, YAMLException } from 'js-yaml';
+import { ChangeEventHandler, useEffect, useState } from 'react';
+import treeify, { TreeObject } from 'treeify';
+
+const defaultData = `hello:
+  world:
+john:
+doe:`;
 
 function App() {
+  const [yml, setYml] = useState(defaultData);
+  const [tree, setTree] = useState('');
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    try {
+      const treeObj = load(yml) as TreeObject;
+      setTree(treeify.asTree(treeObj, false, true));
+      setError(null);
+    } catch (err) {
+      if (err instanceof YAMLException) {
+        setError(`Invalid yaml format: ${err.reason}`);
+      } else {
+        setError('Something went wrong');
+        console.error(err);
+      }
+    }
+  }, [yml]);
+
+  const handleTextChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setYml(e.target.value);
+  };
+
+  const handleCopyToClipboard = () => navigator.clipboard.writeText(tree);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <textarea onChange={handleTextChange} value={yml} cols={50} rows={10} />
+
+      {error && <span>{error}</span>}
+
+      <div>
+        <button onClick={handleCopyToClipboard}>Copy</button>
+
+        <pre>{tree}</pre>
+      </div>
+    </main>
   );
 }
 
